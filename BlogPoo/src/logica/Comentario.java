b347a75a577c407ad629942d896d413884c6489b
Feppoo <167021596+Feppoo@users.noreply.github.com>
2026-04-28 15:37:58 -0600

    Cambios a clase Comentario y Publicacion

--- BlogPoo/src/logica/Comentario.java
+++ BlogPoo/src/logica/Comentario.java
@@ -1,5 +1,41 @@
 package logica;
 
+import java.time.LocalDateTime;
+
 public class Comentario {
+	private LocalDateTime fechaCreacion;
+	private String autor;
+	private String dirIP;
+	private String texto;
+	
+	public Comentario(String autor, String dirIP, String texto) {
+		fechaCreacion = LocalDateTime.now();
+		this.autor = autor;
+		this.dirIP = dirIP;
+		this.texto = texto;
+	}
+
+	public LocalDateTime getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public String getDirIP() {
+		return dirIP;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	@Override
+	public String toString() {
+		return "Comentario [fechaCreacion=" + fechaCreacion + ", autor=" + autor + ", dirIP=" + dirIP + ", texto="
+				+ texto + "]";
+	}
+	
 
 }
--- BlogPoo/src/logica/Publicacion.java
+++ BlogPoo/src/logica/Publicacion.java
@@ -1,5 +1,55 @@
 package logica;
 
-public class Publicacion {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Publicacion {
+	private List<Comentario> comentarios = new ArrayList<>();
+	private static int cantPubs;
+	private int codPub;
+	private String titulo;
+	private String texto;
+	private String creador;
+	private LocalDateTime fechaPub;
+	
+	public Publicacion(String titulo, String texto, String creador) {
+		fechaPub = LocalDateTime.now();
+		this.titulo = titulo;
+		this.texto = texto;
+		this.creador = creador;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public String getCreador() {
+		return creador;
+	}
+
+	public LocalDateTime getFechaPub() {
+		return fechaPub;
+	}
+	
+	public boolean agregarCom(Comentario com) {
+		System.out.println("Por implementar...");
+		return true;
+	}
+	
+	public boolean borrarCom(Comentario com) {
+		System.out.println("Por implementar...");
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Publicacion [comentarios=" + comentarios + ", codPub=" + codPub + ", titulo=" + titulo + ", texto="
+				+ texto + ", creador=" + creador + ", fechaPub=" + fechaPub + "]";
+	}
+	
 }
